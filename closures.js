fdd7ff7f0281f25483d9952e685528b352821158
shelbybjensen <shelbybjensen@gmail.com>
2015-09-23 20:00:09 -0600

    1st problem

--- closures.js
+++ closures.js
@@ -9,7 +9,9 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+  var inner = outer();
+  console.log(inner());
+
 
 //Once you do that, invoke inner.
 
